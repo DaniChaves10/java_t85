6231c4c5569ce38edce5826285b24fef1547ce56
Daniel Araujo Chaves <danielaraujo.da789@gmail.com>
2026-06-12 10:46:24 -0300

    Finalizando a aula 02

---
+++ aula_02_operadores/src/aula_02_operadores/OperadoresUnarios.java
@@ -0,0 +1,18 @@
+package aula_02_operadores;
+
+public class OperadoresUnarios {
+	
+	public static void main(String[] args) {
+		
+		int contador = 1000;
+		
+		System.out.println("Valor inicial de contador: " + contador);
+		
+		//System.out.println("Valor pré Incrementado: " + (++ contador));
+		
+		System.out.println("Valor pós Incrementado: " + (contador ++));
+		
+		System.out.println("Novo valor Incrementado: " + contador);
+
+	}
+}
